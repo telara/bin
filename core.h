dbd6c8b981ce08df969e13268cd5ac8444d1b72a
taraere <taraelsen@gmail.com>
2021-02-11 10:35:02 +0100

    first commit

---
+++ core.h
@@ -0,0 +1,11 @@
+#ifndef __CORE_H
+#define __CORE_H
+
+extern int gcd(int a, int b);
+extern int fac(int n);
+extern int fib(int n);
+extern bool isprime(int n);
+
+extern void printBool(bool b);
+
+#endif
---
+++ coreio.h
@@ -0,0 +1,8 @@
+#ifndef __COREIO_H
+#define __COREIO_H
+
+/* extern int arr[] fibs(int n); */
+extern int fibs(int n);
+extern int primes(int n);
+
+#endif
