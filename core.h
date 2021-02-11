e4e1694db7133de142e3ca80ea90e62c757e45dc
taraere <taraelsen@gmail.com>
2021-02-11 11:50:50 +0100

    primes function added

--- core.h
+++ core.h
@@ -5,7 +5,6 @@ extern int gcd(int a, int b);
 extern int fac(int n);
 extern int fib(int n);
 extern bool isprime(int n);
-
-extern void printBool(bool b);
+extern int testisprime(int n);
 
 #endif
--- coreio.h
+++ coreio.h
@@ -1,7 +1,6 @@
 #ifndef __COREIO_H
 #define __COREIO_H
 
-/* extern int arr[] fibs(int n); */
 extern int fibs(int n);
 extern int primes(int n);
 
